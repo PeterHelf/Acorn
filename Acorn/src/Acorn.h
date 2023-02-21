35e03973d8c0a2d5a14a151428caf487ad6b645c
PeterHelf <106546@fhwn.ac.at>
2023-02-21 13:05:08 +0100

    added engine entry point

---
+++ Acorn/src/Acorn.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Acorn applications
+
+#include "Acorn/Application.h"
+
+// ---Entry Point--------------------
+#include "Acorn/EntryPoint.h"
+// ----------------------------------
---
+++ Acorn/src/Acorn/Application.cpp
@@ -0,0 +1,20 @@
+#include "Application.h"
+
+namespace Acorn
+{
+	Application::Application()
+	{
+	}
+
+	Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		while (true)
+		{
+
+		}
+	}
+}
