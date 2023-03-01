db62e87673c5c73b0af89d6d16eecf5e83f6ee1a
PeterHelf <106546@fhwn.ac.at>
2023-03-01 02:41:39 +0100

    added logging

--- Acorn/src/Acorn/EntryPoint.h
+++ Acorn/src/Acorn/EntryPoint.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "Log.h"
+
 #ifdef AC_PLATFORM_WINDOWS
 
 extern Acorn::Application* Acorn::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Acorn::Log::Init();
+	AC_CORE_WARN("Initialized Log!");
+	AC_INFO("Hello!");
+
 	auto app = Acorn::CreateApplication();
 	app->Run();
 	delete app;
