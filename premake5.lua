f6f78cecc2283796150ab8b48e97e8d1f175ae38
PeterHelf <106546@fhwn.ac.at>
2023-03-16 02:06:00 +0100

    added precompiled headers

--- premake5.lua
+++ premake5.lua
@@ -17,6 +17,9 @@ project "Acorn"
 	
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	pchheader "acpch.h"
+	pchsource "Acorn/src/acpch.cpp"
 
 	files
 	{
